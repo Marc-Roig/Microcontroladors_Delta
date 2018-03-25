e452cd44c8128159fc648cbc2a044b70d4dbead9
Marc Roig <marc12info@gmail.com>
2018-03-25 21:48:27 +0200

    Servo test done
    The mg996 servos have some slack, feedback through potentiometer is needed to know exactly where the axis is positioned.
    
    With this feedback the delta has to be able to correct the offset generated by this slack on the fly.
    
    Save the ohm resistance of 4 or 5 angles for every servo.

--- DeltaArduinoTests/DeltaArduinoTests.ino
+++ DeltaArduinoTests/DeltaArduinoTests.ino
@@ -5,10 +5,10 @@
 
 Servo servo;
 
-int max_duty_cycle = 2200;
-int min_duty_cycle = 860;
+int max_duty_cycle = 2400;
+int min_duty_cycle = 600;
 
-int change_mode_button_pin = 09;
+int change_mode_button_pin =  9;
 int increase_dc_button_pin = 10;
 int decrease_dc_button_pin = 11;
 int change_step_change_pin = 12;
@@ -16,90 +16,103 @@ int change_step_change_pin = 12;
 
 void setup() {
 
-	Serial.begin(9600);
+    Serial.begin(9600);
 
-	servo.attach(10);
+    servo.attach(6);
 
-	pinMode(change_mode_button_pin, INPUT);
-	pinMode(increase_dc_button_pin, INPUT);
-	pinMode(decrease_dc_button_pin, INPUT);
-	pinMode(change_step_change_pin, INPUT);
+    pinMode(change_mode_button_pin, INPUT);
+    pinMode(increase_dc_button_pin, INPUT);
+    pinMode(decrease_dc_button_pin, INPUT);
+    pinMode(change_step_change_pin, INPUT);
 
 }
 
 void loop() {
 
-	static int duty_cycle = (min_duty_cycle + max_duty_cycle) / 2; // Start at the middle
-	static int change_dc_mode = CHANGE_WITH_POTENCIOMETER;
+    static int duty_cycle = (min_duty_cycle + max_duty_cycle) / 2; // Start at the middle
+    static int change_dc_mode = CHANGE_WITH_BUTTONS;
 
-	switch (change_dc_mode) {
+    switch (change_dc_mode) {
 
-		case CHANGE_WITH_POTENCIOMETER:		potenciometer(&duty_cycle);
-											break;
+        case CHANGE_WITH_POTENCIOMETER:     potenciometer(&duty_cycle);
+                                            break;
 
-		case CHANGE_WITH_BUTTONS:			push_buttons(&duty_cycle, &change_dc_mode);
-											break;
+        case CHANGE_WITH_BUTTONS:           push_buttons(&duty_cycle);
+                                            break;
 
-	} 
+    } 
+    
+    change_mode(&change_dc_mode);
+    
+    servo.writeMicroseconds(duty_cycle);
 
-	servo.writeMicroseconds(duty_cycle);
-
-	serial_write_every_ms(500, int_to_char(duty_cycle));
-
+    serial_write_every_ms(1000, duty_cycle);
+  
 }
 
-void serial_write_every_ms(int wait_time, char duty_cycle[]) {
+void serial_write_every_ms(int wait_time, int duty_cycle) {
 
-	static unsigned long startMilis = millis();
-
-	if ((millis() - startMilis) > wait_time) {
-		startMilis = millis();
-		Serial.write(duty_cycle);
-	}
+    static unsigned long startMilis = millis();
+    
+    if ((millis() - startMilis) > wait_time) {
+        startMilis = millis();
+        Serial.println(duty_cycle);
+    }
 
 }
 
 char* int_to_char(int number) {
-	
-	int number_len = 1;
-	int temp = number;
+    
+    int number_len = 1;
+    int temp = number;
 
-	while(temp > 9) {
-		temp /= 10;
-		number_len++;
-	}
+    while(temp > 9) {
+        temp /= 10;
+        number_len++;
+    }
 
-	char* val = (char*) malloc(number_len);
+    char* val = (char*) malloc(number_len+2);
 
-	for (int i = 0; i < number_len; i++) {
+    for (int i = 0; i < number_len; i++) {
 
-		val[i] = number / (int)(pow(10, (number_len-i-1))) % 10 + '0';
+        val[i] = number / (int)(pow(10, (number_len-i-1))) % 10 + '0';
 
-	}
+    }
+    
+    val[number_len] = '\n';
+    val[number_len+1] = '\0';
 
-	char* result = val;
-	free(val); // Dont know if program free's it when exits function
-
-	return result;
+    return val;
 }
-
-
 
 void potenciometer(int* duty_cycle) {
 
-	*duty_cycle = map(analogRead(A0) ,0, 1023, min_duty_cycle, max_duty_cycle);
+    *duty_cycle = map(analogRead(A0) ,0, 1023, min_duty_cycle, max_duty_cycle);
 
 }
 
-void push_buttons(int* duty_cycle, int* change_dc_mode) {
+void change_mode(int* change_dc_mode) {
 
-    static int S1 = 0, S2 = 0, S3 = 0; S4 = 0;
+    static int S4 = 0;
+    bool change_dc_m_button = digitalRead(change_mode_button_pin);
+    
+    if (change_dc_m_button && !S4) {
+    
+      *change_dc_mode = (*change_dc_mode + 1) % 2;
+      S4 = 1;
+      
+    }
+    else if (!change_dc_m_button) S4 = 0;
+}
+
+void push_buttons(int* duty_cycle) {
+
+    static int S1 = 0, S2 = 0, S3 = 0;
     static int mode = 0; 
 
     int step1 = 20, step2 = 40, step3 = 100;
 
-    bool change_mode_button = digitalRead(change_mode_button_pin);
-    bool change_dc_m_button = digitalRead(change_mode_button_pin);
+    bool change_mode_button = digitalRead(change_step_change_pin);
     bool increase_dc_button = digitalRead(increase_dc_button_pin);
     bool decrease_dc_button = digitalRead(decrease_dc_button_pin);
  
@@ -121,8 +134,9 @@ void push_buttons(int* duty_cycle, int* change_dc_mode) {
      if (*duty_cycle >= max_duty_cycle) *duty_cycle = max_duty_cycle;
 
      S1 = 1;
+
     }
-    else if (!decrease_dc_button) S1 = 0;
+    else if (!increase_dc_button) S1 = 0;
 
     //--DECREMENT duty_cycle--//
     if (decrease_dc_button && !S2) {
@@ -149,12 +163,10 @@ void push_buttons(int* duty_cycle, int* change_dc_mode) {
     //--CHANGE STEP--//
     if (change_mode_button && !S3) {
 
-		mode = (mode + 1) % 3; 
-		S3 = 1;
+        mode = (mode + 1) % 3; 
+        S3 = 1;
 
     }
     else if (!change_mode_button) S3 = 0;
-
-    if (change_dc_m_button && !S4)
 
 }
