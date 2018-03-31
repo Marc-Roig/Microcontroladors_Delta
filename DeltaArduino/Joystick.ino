13adc1e263694f8c0e462d39a81351c771fafdd0
Marc Roig <marc12info@gmail.com>
2018-03-31 12:30:56 +0200

    Added timer

--- DeltaArduino/Joystick.ino
+++ DeltaArduino/Joystick.ino
@@ -1,16 +1,40 @@
 #include "Config.h"
 
+/*********************************************************************
+* Function: joystick_change_xy();
+*
+* Overview: Move X and Y of end effector with the joystick.
+*			Speed of end effector can be calculated like this:
+*			X_speed = (joys_x * base_speed) / time_difference_ms  
+* 			In the end the speed that will matter be each servo's speed.
+* 			
+* PreCondition: none
+*
+* Input: none
+*
+* Output: none
+*
+********************************************************************/
+
 void joystick_change_xy() {
 
+	static int StartTime = millis();
+
+	int time_difference_ms = 200; // Every 200 ms program will read the joystick val 
 	// int joystick_number_of_speeds = 10;
 
-	float joys_x = map(analogRead(JOYSTICK_X_PIN), 0, 1023, -JOYSTICK_NUMBER_OF_SPEEDS/2, JOYSTICK_NUMBER_OF_SPEEDS/2);
-	float joys_y = map(analogRead(JOYSTICK_Y_PIN), 0, 1023, -JOYSTICK_NUMBER_OF_SPEEDS/2, JOYSTICK_NUMBER_OF_SPEEDS/2);
+	if ((millis() - StartTime) > time_difference_ms ) {
 
-	float base_speed_increment = 0.1;
+		float joys_x = map(analogRead(JOYSTICK_X_PIN), 0, 1023, -JOYSTICK_NUMBER_OF_SPEEDS/2, JOYSTICK_NUMBER_OF_SPEEDS/2);
+		float joys_y = map(analogRead(JOYSTICK_Y_PIN), 0, 1023, -JOYSTICK_NUMBER_OF_SPEEDS/2, JOYSTICK_NUMBER_OF_SPEEDS/2);
 
-	deltainfo.x += joys_x * base_speed; 
-	deltainfo.y += joys_y * base_speed; 
+		float base_speed_increment = 0.1;
+
+		deltainfo.x += joys_x * base_speed; 
+		deltainfo.y += joys_y * base_speed; 
+
+		StartTime = millis();
+	}
 
 }
 
